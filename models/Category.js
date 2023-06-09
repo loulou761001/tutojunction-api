252e2f50f2f93e13d9dbb42f6e178a2230be7f7d
loulou761001 <louis.cherik@gmail.com>
2023-06-09 11:10:58 +0200

    added category model

---
+++ models/Category.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = require("mongodb");
+
+const CategoryModel = mongoose.Schema({
+  created_at: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+  updated_at: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+
+  parent: { type: ObjectId },
+  subs: [
+    {
+      category: { type: ObjectId },
+    },
+  ],
+});
+
+module.exports = category = mongoose.model("categories", CategoryModel);
